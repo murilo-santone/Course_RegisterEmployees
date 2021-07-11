3abc2f7a7317e751d89469b2ff91eee3645c97b2
murilo <murilo.santone@hotmail.com>
2021-07-11 00:53:39 -0300

    Course CSharp - Employee and OutsourcedEmployee

---
+++ Course_empresa/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_empresa.Entities
+{
+    // Funcionário terceirizado é um funcionário também
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;  // 1.1 = 110%
+        }
+
+    }
+}
